021115286a6c4df77d3e820c2b13f1275a65780b
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-04-12 11:57:11 +0000

    Updating packages

--- open-garage-blynk.groovy
+++ open-garage-blynk.groovy
@@ -26,3 +26,8 @@ metadata {
 		capability "Garage Door Control"
 		capability "Momentary"
 		capability "Polling"
+		capability "Refresh"
+		capability "Switch"
+
+		attribute "lastHttpStatus", "enum", ["succeeded", "failed"]
+	}
