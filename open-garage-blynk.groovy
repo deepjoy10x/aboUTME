4eeff0f317d066ad525d6299754f1793c5de0ef4
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-04-12 07:48:39 +0000

    eol

--- open-garage-blynk.groovy
+++ open-garage-blynk.groovy
@@ -1,3 +1,11 @@
 
 /**
  *  SmartThings device handler for OpenGarage via Blynk.
+ *
+ *  Copyright 2018 Yuxuan "fishy" Wang
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
