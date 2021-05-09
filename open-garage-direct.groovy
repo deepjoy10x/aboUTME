2563e4033d9110a7fa38c855759e5ad1600b9dc3
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-05-09 22:52:29 +0000

    Change titles

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -4,4 +4,4 @@
  *  Copyright 2018 Yuxuan "fishy" Wang
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of th
+ *  in compliance with the License. You may obtain a copy of the Lic
