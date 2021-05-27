b3bd7ec6b1516eb8e6a3d52e9140e51ca158723b
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-05-27 15:51:30 +0000

    NR-501 : Media Archive user interface implementation version 1.0

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -13,4 +13,4 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *
- *  Some of the code are from https://github.com/adamheinmille
+ *  Some of the code are from https://github.com/adamheinmiller/ST_MyQ by Ad
