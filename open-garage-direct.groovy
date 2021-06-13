6ae2642636055d8f6dee982c686936dbfa2d8057
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-06-13 06:36:45 +0000

    a bit of documentation

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -32,4 +32,4 @@ metadata {
 		capability "Sensor"
 
 		attribute "lastHttpStatus", "enum", ["succeeded", "failed"]
-		attribute "carStatus", "enum", ["pr
+		attribute "carStatus", "enum", ["present",
