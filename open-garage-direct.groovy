05e14f8538f0685e57555ec497bdc2c3ccc67183
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-06-28 17:15:54 +0000

    new projects

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -50,4 +50,4 @@ metadata {
 			name: "http_prefix",
 			type: "text",
 			title: "HTTP(s) prefix",
-			description: "Example: \"http://yourip:8080\" or \"https://opengarage.myhome
+			description: "Example: \"http://yourip:8080\" or \"https://opengarage.myhome.com\
