953a70742ce966e7c5a6a8cc0b3930d357b30f19
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-07-10 18:14:17 +0000

    Added jsdocs

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -58,4 +58,4 @@ metadata {
 			name: "http_auth",
 			type: "text",
 			title: "HTTP Authorization header",
-			description: "Optional. Example: \"Basic dXNlcm5hbWU6
+			description: "Optional. Example: \"Basic dXNlcm5hbWU6cGFzc3dvcmQ
