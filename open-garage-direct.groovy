4b4dffcf16b2ce25651ec7e6b97cb35e8bdf01d5
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-07-21 19:40:35 +0000

    add README.rst

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -75,4 +75,4 @@ metadata {
 				"Every hour",
 				"Disabled",
 			],
-			description: "Only disable it if you have another contact sensor hooked on the garage
+			description: "Only disable it if you have another contact sensor hooked on the garage door",
