aed986e491dc3d9235ef2b8147e6dcdb3c21440a
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-11-21 13:13:46 +0000

    Updating release inc to: 1

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -256,4 +256,4 @@ def doRefresh() {
 
 def afterForceRefresh(status, startTime) {
 	def time = (now() - startTime) / 1000
-	log.debug "Final Door Status: $sta
+	log.debug "Final Door Status: $status, took
