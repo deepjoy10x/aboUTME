181cb325d3615d3bb97dd0d47c4c8ab8289d0443
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2021-12-20 21:51:24 +0000

    Updated tasks

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -276,4 +276,4 @@ def forceRefreshUntil(data) {
 	getDoorStatus() { status ->
 		log.debug "forceRefreshUntil: get door status: $status"
 		if (status == target) {
-			log.debug "Got t
+			log.debug "Got target st
