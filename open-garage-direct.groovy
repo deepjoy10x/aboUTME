fd17c9ff5ccdf0add967d78fbe4ed5fd388b7aae
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-03-18 14:42:33 +0000

    added new projects

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -383,4 +383,4 @@ def getDoorStatus(callback = {}) {
 			descriptionText: "Read count is $rcnt"
 		)
 
-		log.debug "getDoorStatus: value = $value, state = $state, carValue = $c
+		log.debug "getDoorStatus: value = $value, state = $state, carValue = $carVal
