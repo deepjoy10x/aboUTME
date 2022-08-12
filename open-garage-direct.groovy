b773961bdce1ce441772d290b4b507764f44ee35
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-08-12 13:33:41 +0000

    cosmetic tweaks

--- open-garage-direct.groovy
+++ open-garage-direct.groovy
@@ -497,4 +497,4 @@ def callApiGet(apipath, query, callback) {
 			name: "lastHttpStatus",
 			value: "failed",
 			displayed: true,
-			descriptionText: "HTTP req
+			descriptionText: "HTTP request failed",
