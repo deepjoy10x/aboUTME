1f709346f3ab8cbaf2b5c57566a5ef985c591185
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-08-24 17:02:35 +0000

    Updated doctrine/common dependency to ~2.5

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -3,4 +3,5 @@
  *
  *  Copyright 2016 Yuxuan Wang
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file ex
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in c
