9b56103432f9e66b152b988adcc7d09ca6688f9e
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-08-24 09:03:49 +0000

    2.7.4

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -3,4 +3,4 @@
  *
  *  Copyright 2016 Yuxuan Wang
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use t
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file ex
