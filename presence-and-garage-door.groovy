5f5dd67185c31dd5399455ddeb594228c8251e78
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-11-20 09:33:08 +0000

    poke

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -89,4 +89,4 @@ def initialize() {
 
 def doorHandler(evt) {
 	log.debug "doorHandler: $evt.value: $evt, $settings"
-	if ("closing" == evt.value || "closed" == evt
+	if ("closing" == evt.value || "closed" == evt.valu
