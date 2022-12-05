1f5cf4bb25ef254eb28bd876202581117b316ab6
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-12-05 22:05:30 +0000

    fixed build script

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -104,4 +104,4 @@ def contactHandler(evt) {
 def presenceHandler(evt) {
 	def carName = evt.getDevice().displayName
 	log.debug "presenceHandler: $evt.value, $carName, $state, $settings"
-	def doorSt
+	def doorState = doo
