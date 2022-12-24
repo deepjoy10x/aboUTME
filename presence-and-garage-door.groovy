980dcf522ecfd7b3f332ba4594eb6bfee5f96384
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-12-24 16:00:04 +0000

    Improving test coverage

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -116,4 +116,6 @@ def presenceHandler(evt) {
 			def msg = "NOT auto opening ${door.displayName} as we didn't auto close it earlier"
 			log.debug "${msg}"
 			if (phone) {
-				sendSms(phone, ms
+				sendSms(phone, msg)
+			}
+	
