5ab7b5c2c4581546c675d70baf5da265f7cabfac
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2022-12-24 06:02:45 +0000

    Trailing whitespace

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -116,4 +116,4 @@ def presenceHandler(evt) {
 			def msg = "NOT auto opening ${door.displayName} as we didn't auto close it earlier"
 			log.debug "${msg}"
 			if (phone) {
-				sendSm
+				sendSms(
