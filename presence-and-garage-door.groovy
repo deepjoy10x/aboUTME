4bee159330281f7ed2b5a02d6bdf8b6c113b14f8
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2023-01-10 07:38:34 +0000

    Revert "Revert "Update README.md""

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -130,4 +130,4 @@ def presenceHandler(evt) {
 		state.shouldOpen = shouldClose
 		if (shouldClose) {
 			log.debug "Closing at ${state.lastClose}"
-			def msg = "Closing ${door.disp
+			def msg = "Closing ${door.displayName
