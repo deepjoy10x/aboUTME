07ef7c9b8238b275be2e1042e97a742970420fad
deepjoy10x <73684961+deepjoy10x@users.noreply.github.com>
2023-02-10 09:15:06 +0000

    Update bootstrap.css

--- presence-and-garage-door.groovy
+++ presence-and-garage-door.groovy
@@ -139,3 +139,4 @@ def presenceHandler(evt) {
 			def msg = "NOT auto closing as ${door.displayName} was ${doorState} and last closing was ${sec}s ago"
 			log.debug "${msg}"
 			if (phone) {
+				sendS
